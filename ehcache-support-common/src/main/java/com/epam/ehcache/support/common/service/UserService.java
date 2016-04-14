965b699d266c9c26b4ab6498e97be175f5bff414
Andrei-Khadziukou <Andrei_Khadziukou@epam.com>
2016-04-14 19:09:46 +0300

    Organize project structure to run two instance of application

---
+++ ehcache-support-common/src/main/java/com/epam/ehcache/support/common/service/UserService.java
@@ -0,0 +1,45 @@
+package com.epam.ehcache.support.common.service;
+
+import com.epam.ehcache.support.common.domain.User;
+import com.epam.ehcache.support.common.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Date: 04/13/2016
+ *
+ * @author Andrei_Khadziukou
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(String firstName, String lastName) {
+        User user = new User(UUID.randomUUID().toString());
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        userRepository.insert(user);
+        return user;
+    }
+
+    public List<User> findUsers() {
+        return userRepository.find();
+    }
+
+    public User find(String id) {
+        return userRepository.find(id);
+    }
+
+    public void updateUser(User user) {
+        userRepository.update(user);
+    }
+
+    public void delete(String id) {
+        userRepository.delete(id);
+    }
+}
